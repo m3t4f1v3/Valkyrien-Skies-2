299aa87dae64a6162d2c8778c14bd50b1daa4c2b
ThePlasticPotato <theplasticpotato@gmail.com>
2025-07-04 18:04:59 -0400

    replace redirect with overwrite because that doesn't work like that you silly billy

--- common/src/main/java/org/valkyrienskies/mod/mixin/feature/container_distance_check/MixinContainer.java
+++ common/src/main/java/org/valkyrienskies/mod/mixin/feature/container_distance_check/MixinContainer.java
@@ -1,25 +1,30 @@
 package org.valkyrienskies.mod.mixin.feature.container_distance_check;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.Container;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.valkyrienskies.mod.common.VSGameUtilsKt;
 
 @Mixin(Container.class)
 public interface MixinContainer {
-
-    @Redirect(
-        method = "stillValidBlockEntity(Lnet/minecraft/world/level/block/entity/BlockEntity;Lnet/minecraft/world/entity/player/Player;I)Z",
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/world/entity/player/Player;distanceToSqr(DDD)D"
-        )
-    )
-    private static double includeShipsInDistanceCheck(
-        final Player receiver, final double x, final double y, final double z) {
-        return VSGameUtilsKt.squaredDistanceToInclShips(receiver, x, y, z);
+    /**
+     * @author Potato
+     * @reason This mixin literally doesn't work as a redirect, so we have to overwrite it. Damn you ASM!
+     */
+    @Overwrite
+    static boolean stillValidBlockEntity(BlockEntity blockEntity, Player player, int i) {
+        Level level = blockEntity.getLevel();
+        BlockPos blockPos = blockEntity.getBlockPos();
+        if (level == null) {
+            return false;
+        } else if (level.getBlockEntity(blockPos) != blockEntity) {
+            return false;
+        } else {
+            return VSGameUtilsKt.squaredDistanceToInclShips(player, (double)blockPos.getX() + (double)0.5F, (double)blockPos.getY() + (double)0.5F, (double)blockPos.getZ() + (double)0.5F) <= (double)(i * i);
+        }
     }
-
 }
