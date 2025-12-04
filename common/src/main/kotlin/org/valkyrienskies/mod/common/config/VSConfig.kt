d72b05ea865a0a77545b8e1cb785c27d5d946fde
James Green <jgreenop22@gmail.com>
2025-12-03 19:27:54 -0800

    update vscore

--- common/src/main/kotlin/org/valkyrienskies/mod/common/config/VSConfig.kt
+++ common/src/main/kotlin/org/valkyrienskies/mod/common/config/VSConfig.kt
@@ -2,9 +2,9 @@ package org.valkyrienskies.mod.common.config
 
 import net.minecraftforge.common.ForgeConfigSpec
 import net.minecraftforge.fml.config.ModConfig
-import org.valkyrienskies.core.internal.config.ConfigModelCategory
-import org.valkyrienskies.core.internal.config.ConfigModelEntry
-import org.valkyrienskies.core.internal.config.VSConfigModel
+import org.valkyrienskies.core.internal.config.VsiConfigModel
+import org.valkyrienskies.core.internal.config.VsiConfigModelCategory
+import org.valkyrienskies.core.internal.config.VsiConfigModelEntry
 import org.valkyrienskies.mod.common.ValkyrienSkiesMod
 
 object VSConfig {
@@ -12,17 +12,17 @@ object VSConfig {
     private val core_server_config = ValkyrienSkiesMod.vsCore.getServerConfig()
     val CORE_SERVER_SPEC: ForgeConfigSpec = buildCategory(core_server_config.root, ForgeConfigSpec.Builder()).build()
 
-    private fun buildCategory(configCategory: ConfigModelCategory, builder: ForgeConfigSpec.Builder ): ForgeConfigSpec.Builder {
+    private fun buildCategory(configCategory: VsiConfigModelCategory, builder: ForgeConfigSpec.Builder ): ForgeConfigSpec.Builder {
         for (node in configCategory.children) {
             val entry = node.value
-            if (entry is ConfigModelCategory) {
+            if (entry is VsiConfigModelCategory) {
                 builder.push(entry.title)
                 buildCategory(entry, builder)
                 builder.pop()
-            } else if (entry is ConfigModelEntry<*>){
+            } else if (entry is VsiConfigModelEntry<*>){
                 fun <T> define(v: T) = builder.define<T>(entry.name, v)
 
-                when (val v = entry.value) {
+                when (val v = entry.getValue()) {
                     is Int -> builder.defineInRange(entry.name, v, entry.min as Int, entry.max as Int)
                     is Double -> builder.defineInRange(entry.name, v, entry.min as Double, entry.max as Double)
                     is Boolean -> define(v)
@@ -40,14 +40,14 @@ object VSConfig {
     fun update(config: ModConfig) {
         core_server_config.update(config)
     }
-}
 
-internal fun VSConfigModel.update(forgeConfig: ModConfig) {
-    forEachEntry { path, node ->
-        forgeConfig.configData.get<Any>("$path${node.name}")?.let { newValue ->
-            if (node.default != null && node.default!!::class.isInstance(newValue)) {
-                @Suppress("UNCHECKED_CAST")
-                (node as ConfigModelEntry<Any>).setValue(newValue)
+    private fun VsiConfigModel.update(forgeConfig: ModConfig) {
+        forEachEntry { path, node ->
+            forgeConfig.configData.get<Any>("$path${node.name}")?.let { newValue ->
+                if (node.default != null && node.default!!::class.isInstance(newValue)) {
+                    @Suppress("UNCHECKED_CAST")
+                    (node as VsiConfigModelEntry<Any>).setValue(newValue)
+                }
             }
         }
     }
