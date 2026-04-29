360bd5a8d06b10f77e2b63e5ae7d5b8b2a9b0a05
ThePlasticPotato <theplasticpotato@gmail.com>
2026-04-29 14:43:49 -0400

    oopsie

--- common/src/main/java/org/valkyrienskies/mod/mixin/client/world/MixinClientChunkCache.java
+++ common/src/main/java/org/valkyrienskies/mod/mixin/client/world/MixinClientChunkCache.java
@@ -31,6 +31,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.valkyrienskies.core.api.ships.ClientShip;
 import org.valkyrienskies.core.api.ships.properties.ChunkClaim;
