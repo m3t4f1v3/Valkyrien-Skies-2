3c2cdfe7833263eba69a5bcc07e9622200a9d30d
Bunting_chj <chj990707@postech.ac.kr>
2026-05-26 22:09:34 +0900

    Re-positioned uniform setter injection.

--- common/src/main/java/org/valkyrienskies/mod/mixin/mod_compat/sodium/MixinLevelRenderer.java
+++ common/src/main/java/org/valkyrienskies/mod/mixin/mod_compat/sodium/MixinLevelRenderer.java
@@ -3,14 +3,10 @@ package org.valkyrienskies.mod.mixin.mod_compat.sodium;
 import com.mojang.blaze3d.vertex.PoseStack;
 import java.util.Map;
 import net.minecraft.client.Camera;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.LightTexture;
-import net.minecraft.client.renderer.RenderBuffers;
 import net.minecraft.client.renderer.ShaderInstance;
-import net.minecraft.client.renderer.blockentity.BlockEntityRenderDispatcher;
-import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import org.joml.Matrix4f;
 import org.joml.Vector3f;
 import org.spongepowered.asm.mixin.Mixin;
@@ -23,21 +19,6 @@ import org.valkyrienskies.mod.mixin.accessors.client.render.GameRendererAccessor
 @Mixin(LevelRenderer.class)
 public class MixinLevelRenderer {
     @Inject(
-        method = "<init>",
-        at = @At("RETURN")
-    )
-    private void setTextures(Minecraft minecraft, EntityRenderDispatcher entityRenderDispatcher,
-        BlockEntityRenderDispatcher blockEntityRenderDispatcher, RenderBuffers renderBuffers, CallbackInfo ci){
-        Map<String, ShaderInstance> shaders = ((GameRendererAccessor)minecraft.gameRenderer).vs$getShaders();
-        for(ShaderInstance entityShader : shaders.values()) {
-            if(entityShader.getUniform("u_isInWorld") == null) continue;
-            entityShader.getUniform("u_VsLightSections").set(SodiumCompat.LIGHT_SECTIONS_TEXTURE_UNIT);
-            entityShader.getUniform("u_VsLightLut").set(SodiumCompat.LIGHT_LUT_TEXTURE_UNIT);
-            entityShader.getUniform("u_VsShipEmitters").set(SodiumCompat.SHIP_EMITTER_LIST_TEXTURE_UNIT);
-        }
-    }
-
-    @Inject(
         method = "renderLevel",
         at = @At("HEAD")
     )
@@ -46,10 +27,13 @@ public class MixinLevelRenderer {
 
         Vector3f camerPos = camera.getPosition().toVector3f();
         Matrix4f localToCamera = new Matrix4f(poseStack.last().pose()).invert();
-        Map<String, ShaderInstance> shaders = ((GameRendererAccessor)gameRenderer).vs$getShaders();
+        Map<String, ShaderInstance> shaders = ((GameRendererAccessor) gameRenderer).vs$getShaders();
         for(ShaderInstance entityShader : shaders.values()) {
-            if(entityShader.getUniform("u_isInWorld") == null) continue;
+            if (entityShader.getUniform("u_isInWorld") == null) continue;
             entityShader.getUniform("u_VsShipEmitterCount").set(SodiumCompat.getShipEmitterList().size());
+            entityShader.getUniform("u_VsShipEmitters").set(SodiumCompat.SHIP_EMITTER_LIST_TEXTURE_UNIT);
+            entityShader.getUniform("u_VsLightSections").set(SodiumCompat.LIGHT_SECTIONS_TEXTURE_UNIT);
+            entityShader.getUniform("u_VsLightLut").set(SodiumCompat.LIGHT_LUT_TEXTURE_UNIT);
             entityShader.getUniform("u_LocalToCameraRel").set(localToCamera);
             entityShader.getUniform("u_VsRenderOrigin").set(camerPos.x, camerPos.y, camerPos.z);
             entityShader.getUniform("u_isInWorld").set(1);
